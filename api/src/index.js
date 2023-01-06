937a9f636f0753f46b61ee0ed81d232d0f26b3f9
nissaura <cnissara.almeida@outlook.com>
2023-01-06 20:53:57 -0300

    primeiro endpoint feito

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,8 @@
 import 'dotenv/config'
 
+import  usuarioController from './controller/usuarioController.js'
+
+
 import express from 'express'
 import cors from 'cors'
 
@@ -7,5 +10,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+//configuração dos endpoints
+
+server.use(usuarioController);
 
 server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`))
--- api/src/repository/usuarioRepository.js
+++ api/src/repository/usuarioRepository.js
@@ -1,15 +1,15 @@
 import {con } from './connection.js'
 
-export async function login(email,senha) {
+export async function login(email, senha) {
 
     const comando = 
         `select id_usuario 		id,
                 nm_usuario		nome,
-                ds_email			email
+                ds_email		email
             from tb_usuario
-            where ds_email 		= 'admin@admin.com.br'
-            and ds_senha			= '1234' `
+            where ds_email 		= ?
+            and ds_senha		= ? `
 
-    const [linhas ] = await con.query(comando[email,senha])
+    const [linhas] = await con.query(comando, [email, senha])
     return linhas[0];
 }
